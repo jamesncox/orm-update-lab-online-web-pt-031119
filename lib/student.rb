43de74f25f23accca31bf906658294a4388a5566
James Cox <jamesncox@yahoo.com>
2019-05-22 15:42:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
     # def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
       DB[:conn].execute(sql, self.name, self.grade, self.id)
-    end
+    # end
   end
   
   
