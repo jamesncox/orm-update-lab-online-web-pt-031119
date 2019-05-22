266b5b4e3824aa4862b15e3fb51f34f600e1c71e
James Cox <jamesncox@yahoo.com>
2019-05-22 15:31:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,10 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def self.drop_table
+    
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
